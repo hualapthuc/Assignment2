1ef7175815d1eab1f5efe54bc7ad20f0fe5c3877
QuangBach111 <buiquangbach25101996@gmail.com>
2023-05-12 19:19:07 +0700

    homepage data structure

---
+++ js/home-page.js
@@ -0,0 +1,11 @@
+/**
+ * a sẽ gửi 1 json string qua cho em
+ *      var namePoll
+ * 
+ *      var question = {
+ *          question: questionName,
+ *          questionList[]
+ *      }
+ *      
+ *      
+ */
