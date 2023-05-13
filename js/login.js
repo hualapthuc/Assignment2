dec22bfb09f73beca6e63a525b06ce4ff0d669fc
hualapthuc <hua.lap.thuc@gmail.com>
2023-05-13 13:05:03 +0700

    git push

--- js/login.js
+++ js/login.js
@@ -19,3 +19,24 @@
  *          
  *              
  */
+function login() {
+    var username = document.getElementById('alias').value;
+    var password = document.getElementById('password').value;
+    let user_records = new Array();
+        user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+
+    for(var i = 0; i < user_records.length; i++) {
+        if (user_records[i] == null) {
+            alert("User doesn't be existed in the server");
+        }
+        else if ((username == user_records[i].username) && (password == user_records[i].password)){
+            alert("Login successfully");
+        }
+        else if ((username != user_records[i].username) || (password != user_records[i].password)){
+            alert("User name or password is not correct.")
+        }
+        else {
+            alert("There is an error occurring during login section.");
+        }
+    }
+}
